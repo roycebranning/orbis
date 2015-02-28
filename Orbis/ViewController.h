39d1b89e665c609070a6e91b5d815fa4288d7f0c
Michael Krumdick <mrkrumdick@gmail.com>
2015-02-27 22:19:38 -0600

    initial commit

--- Orbis/ViewController.h
+++ Orbis/ViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WhirlyGlobeComponent.h>
 
 @interface ViewController : UIViewController
 
--- Orbis/ViewController.m
+++ Orbis/ViewController.m
@@ -8,15 +8,66 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+{
+    WhirlyGlobeViewController *theViewC;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // From WhirlyGlobe
+    
+    // Create an empty globe and add it to the view
+    theViewC = [[WhirlyGlobeViewController alloc] init];
+    [self.view addSubview:theViewC.view];
+    theViewC.view.frame = self.view.bounds;
+    [self addChildViewController:theViewC];
+    WhirlyGlobeViewController *globeViewC = nil;
+    MaplyViewController *mapViewC = nil;
+    if ([theViewC isKindOfClass:[WhirlyGlobeViewController class]])
+        globeViewC = (WhirlyGlobeViewController *)theViewC;
+    else
+        mapViewC = (MaplyViewController *)theViewC;
+    // we want a black background for a globe, a white background for a map.
+    theViewC.clearColor = (globeViewC != nil) ? [UIColor blackColor] : [UIColor whiteColor];
+    
+    // and thirty fps if we can get it ­ change this to 3 if you find your app is struggling
+    theViewC.frameInterval = 2;
+    
+    // set up the data source
+    MaplyMBTileSource *tileSource =
+    [[MaplyMBTileSource alloc] initWithMBTiles:@"geography-class_medres"];
+    
+    // set up the layer
+    MaplyQuadImageTilesLayer *layer =
+    [[MaplyQuadImageTilesLayer alloc] initWithCoordSystem:tileSource.coordSys
+                                               tileSource:tileSource];
+    layer.handleEdges = (globeViewC != nil);
+    layer.coverPoles = (globeViewC != nil);
+    layer.requireElev = false;
+    layer.waitLoad = false;
+    layer.drawPriority = 0;
+    layer.singleLevelLoading = false;
+    [theViewC addLayer:layer];
+    
+    // start up over San Francisco, center of the universe
+    if (globeViewC != nil)
+    {
+        globeViewC.height = 0.8;
+        [globeViewC animateToPosition:MaplyCoordinateMakeWithDegrees(-122.4192,37.7793)
+                                 time:1.0];
+    } else {
+        mapViewC.height = 1.0;
+        [mapViewC animateToPosition:MaplyCoordinateMakeWithDegrees(-122.4192,37.7793)
+                               time:1.0];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
